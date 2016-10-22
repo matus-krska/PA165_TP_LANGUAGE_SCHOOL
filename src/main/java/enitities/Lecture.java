f00a1e9bd1dd4ec78da17f68f56db92770d9fee3
Matus <matus.krska@gmail.com>
2016-10-22 16:27:28 +0200

    Dependency on javax.persistence Creation of Entity Lecture

---
+++ src/main/java/enitities/Lecture.java
@@ -0,0 +1,40 @@
+package enitities;
+
+import javax.persistence.*;
+import java.util.Date;
+
+/**
+ * Entity Lecture represents a lecture of a course. It is stored as a entry of database table T_LECTURE.
+ * It contains unique identification ID, time of creation, unique code, topic, lecture time and description
+ *
+ * @author Matus Krska, 410073
+ * @since 1.0
+ */
+
+@Entity
+@Table(name="T_LECTURE")
+public class Lecture
+{
+    @Id
+    @Column(name="ID")
+    private Long id;
+
+//    @ManyToOne(fetch=FetchType.LAZY)
+//    @JoinColumn(name="CREATED_BY")
+//    private Lecturer createdBy;
+
+    @Column(name="CODE")
+    private String code;
+
+    @Column(name="TOPIC")
+    private String topic;
+
+    @Column(name="DESCRIPTION")
+    private String description;
+
+    @Column(name="CREATED")
+    private Date created;
+
+    @Column(name="LECTURE_TIME")
+    private Date lectureTime;
+}

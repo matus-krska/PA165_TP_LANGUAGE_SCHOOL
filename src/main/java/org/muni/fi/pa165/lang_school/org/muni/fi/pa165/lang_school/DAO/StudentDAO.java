6df6cc78b632019124e56afa83bbfaf6b34996cb
Šimon Hyben <xhyben@fi.muni.cz>
2016-10-30 14:30:53 +0100

    Correct list of students

--- src/main/java/org/muni/fi/pa165/lang_school/org/muni/fi/pa165/lang_school/DAO/StudentDAO.java
+++ src/main/java/org/muni/fi/pa165/lang_school/org/muni/fi/pa165/lang_school/DAO/StudentDAO.java
@@ -2,7 +2,7 @@ package org.muni.fi.pa165.lang_school.org.muni.fi.pa165.lang_school.DAO;
 
 import java.util.List;
 import javax.persistence.Query;
-import org.muni.fi.pa165.lang_school.entities.Lecture;
+import org.muni.fi.pa165.lang_school.entities.Student;
 import org.muni.fi.pa165.lang_school.genericDAO.GenericDAOImplementation;
 
 /**
@@ -11,7 +11,7 @@ import org.muni.fi.pa165.lang_school.genericDAO.GenericDAOImplementation;
  * @author Richard Zan, 396380
  * @since 1.0
  */
-public class StudentDAO extends GenericDAOImplementation<Lecture, Long>{
+public class StudentDAO extends GenericDAOImplementation<Student, Long>{
     /**
      * Method for finding Student by id, name and surname
      * @param id id/uco of student
@@ -19,7 +19,7 @@ public class StudentDAO extends GenericDAOImplementation<Lecture, Long>{
      * @param surname surname of student
      * @return List of Students that match the query, can be empty
      */
-    public List<Lecture> findByID(String id, String name, String surname)
+    public List<Student> findByID(String id, String name, String surname)
     {
         String query = "SELECT * FROM " + entityClass.getName() + " WHERE ID = :id AND NAME = :name AND SURNAME = :surname";
         Query q = em.createQuery(query);

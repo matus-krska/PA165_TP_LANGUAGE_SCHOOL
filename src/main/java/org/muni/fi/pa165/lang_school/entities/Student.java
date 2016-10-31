a5f5770d82dd08fbcd39277986424bbaf0181fb9
Šimon Hyben <xhyben@fi.muni.cz>
2016-10-31 21:43:03 +0100

    Minor corrections for entities

--- src/main/java/org/muni/fi/pa165/lang_school/entities/Student.java
+++ src/main/java/org/muni/fi/pa165/lang_school/entities/Student.java
@@ -1,15 +1,11 @@
 package org.muni.fi.pa165.lang_school.entities;
 
-import java.io.Serializable;
-import java.util.Date;
 import java.util.List;
-import java.util.Map;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
 

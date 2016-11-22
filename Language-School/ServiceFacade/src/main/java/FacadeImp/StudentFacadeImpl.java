a3acd95ed423113cf9d5ad49f66a855c445e22ed
fRandOmizer <zan.richard@gmail.com>
2016-11-22 20:18:48 +0100

    Bean import

--- Language-School/ServiceFacade/src/main/java/FacadeImp/StudentFacadeImpl.java
+++ Language-School/ServiceFacade/src/main/java/FacadeImp/StudentFacadeImpl.java
@@ -4,10 +4,12 @@ package FacadeImp;
 
 import DTO.StudentDTO;
 import Facade.StudentFacadeInterface;
+import ServiceImp.StudentServiceImpl;
 import java.util.List;
 import java.util.Optional;
 import javax.inject.Inject;
-import org.muni.fi.pa165.lang_school.DAO.StudentDAO;
+import org.muni.fi.pa165.lang_school.entities.Student;
+import org.dozer.Mapper;
 
 
 /**
@@ -15,33 +17,50 @@ import org.muni.fi.pa165.lang_school.DAO.StudentDAO;
  * @since 1.0
  */
 public class StudentFacadeImpl implements StudentFacadeInterface{
-    //@Inject
-    //private Student;
-    
+    @Inject
+    private StudentServiceImpl studentService;
+
+    @Inject
+    private Mapper mapper;
     
     @Override
     public StudentDTO registerStudent(StudentDTO studentDTO) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        Validate.isTrue(studentDTO.getId() == null);
+
+        Student entity = convert(studentDTO);
+        Student saved = hotelService.registerHotel(entity);
+
+        return convert(saved);
     }
 
     @Override
     public StudentDTO updateStudent(StudentDTO studentDTO) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        Validate.notNull(user.getId());
+        UserEntity entity = this.studentDtoToEntity(user);
+        UserEntity updated = userService.updateUser(entity);
+        return this.studentToStudentDto(updated);
     }
 
     @Override
     public Optional<StudentDTO> findById(Long id) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return userService.findByEmail(email).map(this::studentToStudentDto);
     }
 
     @Override
     public List<StudentDTO> filterByNameSurname(String name, String surname) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return userService.findByEmail(email).map(this::studentToStudentDto);
     }
 
     @Override
     public StudentDTO findByIdNameAndSurname(Long id, String name, String surname) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return userService.findByEmail(email).map(this::studentToStudentDto);
+    }
+
+    private Student studentDtoToEntity(StudentDTO dto){
+        return mapper.map(dto,Student.class);
+    }
+    private StudentDTO studentToStudentDto(Student entity){
+        return mapper.map(entity,StudentDTO.class);
     }
     
 }

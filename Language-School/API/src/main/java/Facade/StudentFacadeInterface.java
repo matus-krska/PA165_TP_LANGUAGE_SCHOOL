0433793bc25951a7f34a3355809c827c17fd803d
fRandOmizer <zan.richard@gmail.com>
2016-11-22 18:04:53 +0100

    Oprava maven projektu

--- Language-School/API/src/main/java/Facade/StudentFacadeInterface.java
+++ Language-School/API/src/main/java/Facade/StudentFacadeInterface.java
@@ -1,14 +1,52 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Facade;
 
+import DTO.StudentDTO;
+import java.util.List;
+import java.util.Optional;
+
 /**
+ * Facade interface for access to the students
  * @author Richard Zan, 396380
  * @since 1.0
  */
-public class StudentFacadeInterface {
+public interface StudentFacadeInterface {
+   
+
+    /**
+     * Registers new student
+     * @param studentDTO new room
+     * @return StudentDTO 
+     */
+    public StudentDTO registerStudent(StudentDTO studentDTO);
+
+    /**
+     * Updates existing student
+     * @param studentDTO updated room
+     * @return StudentDTO 
+     */
+    public StudentDTO updateStudent(StudentDTO studentDTO);
+
+    /**
+     * finds student by id
+     * @param id unique id
+     * @return StudentDTO 
+     */
+    public Optional<StudentDTO> findById(Long id);
+
+    /**
+     * Returns filtered users
+     * @param name specifies room criteria
+     * @param surname page info
+     * @return filtered list of studentsDTO
+     */
+    public List<StudentDTO> filterByNameSurname(String name, String surname) ;
     
+     /**
+      * Return StudentDTO
+      * @param id unique id
+      * @param name name of student
+      * @param surname surname of student
+      * @return StudentDTO
+      */
+    public StudentDTO findByIdNameAndSurname(Long id, String name, String surname);
 }
--- Language-School/ServiceFacade/src/main/java/FacadeImp/StudentFacadeImpl.java
+++ Language-School/ServiceFacade/src/main/java/FacadeImp/StudentFacadeImpl.java
@@ -1,14 +1,42 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package FacadeImp;
+
+//import 
+
+import DTO.StudentDTO;
+import Facade.StudentFacadeInterface;
+import java.util.List;
+import java.util.Optional;
+
 
 /**
  * @author Richard Zan, 396380
  * @since 1.0
  */
-public class StudentFacadeImpl {
+public class StudentFacadeImpl implements StudentFacadeInterface{
+
+    @Override
+    public StudentDTO registerStudent(StudentDTO studentDTO) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public StudentDTO updateStudent(StudentDTO studentDTO) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Optional<StudentDTO> findById(Long id) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public List<StudentDTO> filterByNameSurname(String name, String surname) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public StudentDTO findByIdNameAndSurname(Long id, String name, String surname) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
     
 }

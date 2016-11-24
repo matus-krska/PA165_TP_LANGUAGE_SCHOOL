82b6fb23340da4025e51bd1e2822334dc531fa5e
fRandOmizer <zan.richard@gmail.com>
2016-11-24 12:57:20 +0100

    Pom Update

--- Language-School/ServiceFacade/src/test/java/FacadeImp/MapperTest.java
+++ Language-School/ServiceFacade/src/test/java/FacadeImp/MapperTest.java
@@ -4,6 +4,7 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
 import DTO.StudentDTO;
 import javax.inject.Inject;
+import javax.transaction.Transactional;
 import org.dozer.Mapper;
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -11,15 +12,18 @@ import static org.junit.Assert.*;
 import org.junit.runner.RunWith;
 import org.muni.fi.pa165.lang_school.entities.Student;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.TestExecutionListeners;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.transaction.TransactionalTestExecutionListener;
 
 
 /**
  *
  * @author Richard Zan, 396380
  */
-@RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(classes = FacadeImp.MapperTest.class)
+@ContextConfiguration(classes = org.muni.fi.pa165.lang_school.ApplicationContext.class)
+@TestExecutionListeners(TransactionalTestExecutionListener.class)
+@Transactional
 public class MapperTest extends AbstractTestNGSpringContextTests {
 
     @Autowired

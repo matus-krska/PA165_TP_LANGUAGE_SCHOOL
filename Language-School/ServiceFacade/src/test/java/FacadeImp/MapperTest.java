93dda328ad05a2f5cfc1cbbeea275d122d94ebc2
fRandOmizer <zan.richard@gmail.com>
2016-11-24 14:40:51 +0100

    Skoro to funguje

--- Language-School/ServiceFacade/src/test/java/FacadeImp/MapperTest.java
+++ Language-School/ServiceFacade/src/test/java/FacadeImp/MapperTest.java
@@ -1,37 +1,36 @@
 package FacadeImp;
 
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
-import DTO.StudentDTO;
-import javax.inject.Inject;
-import javax.transaction.Transactional;
-import org.dozer.Mapper;
-import org.junit.BeforeClass;
-import org.junit.Test;
-import static org.junit.Assert.*;
-import org.junit.runner.RunWith;
 import org.muni.fi.pa165.lang_school.entities.Student;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.context.TestExecutionListeners;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.transaction.TransactionalTestExecutionListener;
+import DTO.StudentDTO;
+import com.fi.ls.config.BeanMappingConfiguration;
+import javax.transaction.Transactional;
 
+import org.dozer.Mapper;
+import org.dozer.classmap.DozerClass;
+import org.dozer.inject.Inject;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
+import static org.testng.Assert.*;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
 
 /**
  *
  * @author Richard Zan, 396380
  */
-@ContextConfiguration(classes = Mapper.class)
-@TestExecutionListeners(TransactionalTestExecutionListener.class)
-@Transactional
-public class MapperTest extends AbstractTestNGSpringContextTests {
+public class MapperTest {
 
     @Autowired
     private Mapper mapper;
     
-    @Inject
+    @Mock
     Student studentA;
-    @Inject
+    @Mock
     StudentDTO studentADTO;
 
     @BeforeClass
@@ -41,28 +40,26 @@ public class MapperTest extends AbstractTestNGSpringContextTests {
     @Test
     public void testMapStudentToStudentDTO() {
 
-        Student studentA = new Student();
-        studentA.setId(1l);
+        studentA = new Student();
+        
         studentA.setName("Simon");
         studentA.setSurname("Hyben");
 
         StudentDTO dto = mapper.map(studentA, StudentDTO.class);
 
-        assertEquals(studentA.getId(), dto.getId());
         assertEquals(studentA.getName(), dto.getName());
         assertEquals(studentA.getSurname(), dto.getSurname());
     }
 
     @Test
     public void testMapStudentDTOToStudent() {
-        StudentDTO studentADTO = new StudentDTO();
-        studentADTO.setId(1l);
+        studentADTO = new StudentDTO();
+        
         studentADTO.setName("Simon");
         studentADTO.setSurname("Hyben");
 
         Student entity = mapper.map(studentADTO, Student.class);
 
-        assertEquals(studentADTO.getId(), entity .getId());
         assertEquals(studentADTO.getName(), entity .getName());
         assertEquals(studentADTO.getSurname(), entity .getSurname());
     }

e1cdb32a72ad25c82069c9514f15f739ea7b87ab
Matus <matus.krska@gmail.com>
2016-12-14 21:52:10 +0100

    Initalizer

---
+++ Language-School/spring-mvc/src/main/java/Config/Initializer.java
@@ -0,0 +1,37 @@
+package Config;
+
+import org.springframework.web.filter.CharacterEncodingFilter;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+import javax.servlet.Filter;
+
+/**
+ * Initalizer of MVC
+ * @author Matus Krska, 410073
+ * @since 1.0
+ */
+public class Initializer extends AbstractAnnotationConfigDispatcherServletInitializer
+{
+
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class<?>[]{SpringMVCConfig.class};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
+    }
+
+    @Override
+    protected Filter[] getServletFilters() {
+        CharacterEncodingFilter encodingFilter = new CharacterEncodingFilter();
+        encodingFilter.setEncoding("utf-8");
+        return new Filter[]{encodingFilter};
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return null;
+    }
+}
